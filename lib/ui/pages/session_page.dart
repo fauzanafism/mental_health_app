d2de2717515010f7df3ebd27d7f60b015529937e
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-02-20 11:34:07 +0700

    finished session page ui

---
+++ lib/ui/pages/session_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:mental_health_app/common/constant.dart';
+
+import 'package:mental_health_app/ui/widgets/big_box.dart';
+import 'package:mental_health_app/ui/widgets/session_widget.dart';
+
+class Session extends StatelessWidget {
+  const Session({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const BigBox(
+            title: 'Upcoming Session',
+            subtitle:
+                'Sahana V, M.Sc in Clinical Psychology\n\n7:30 PM - 8:30 PM',
+            buttonText: 'Join Now',
+            buttonIcon: Icons.play_circle,
+            color: Color(0xffFEF3E7),
+            isGrey: true,
+            isImage: false),
+        const SizedBox(height: 30),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Text(
+                  'All Sessions',
+                  style: kSubtitle.copyWith(
+                      fontSize: 18, color: const Color(0xff371B34)),
+                ),
+                const Icon(Icons.arrow_drop_down_outlined)
+              ],
+            ),
+            const Icon(
+              Icons.sort,
+              color: kColorGrey,
+            )
+          ],
+        ),
+        const SizedBox(height: 30),
+        const SessionWidget(
+          firstButtonString: 'Reschedule',
+          secondButtonString: 'Join Now',
+        ),
+        const SizedBox(height: 15),
+        const SessionWidget(
+            firstButtonString: 'Re-book', secondButtonString: 'View Profile')
+      ],
+    );
+  }
+}
