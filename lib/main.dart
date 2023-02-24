121aac96737b6d281c77cb1481604fd113ae1b0f
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-02-24 11:37:09 +0700

    wrapped materialapp with screenutilinit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mental_health_app/ui/pages/main_page.dart';
 
 void main() {
@@ -10,11 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mental Health App',
-      initialRoute: '/',
-      routes: {'/': (context) => const MainPage()},
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Mental Health App',
+            initialRoute: '/',
+            routes: {'/': (context) => const MainPage()},
+          );
+        });
   }
 }
