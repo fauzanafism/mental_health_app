66e86da5e2e7fd7482917def54444a3680a36693
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2023-02-24 11:37:30 +0700

    finished chat page ui

--- lib/ui/pages/chat_page.dart
+++ lib/ui/pages/chat_page.dart
@@ -6,32 +6,84 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 6 / 8 + 10,
+      child: Stack(
         children: [
-          ListTile(
-            leading: const CircleAvatar(
-                backgroundImage: AssetImage('assets/images/pp.jpeg')),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Fauzan Nafis M',
-                  style: kBodyTextRubik.copyWith(
-                      fontSize: 14, fontWeight: FontWeight.w600),
+          Column(
+            children: const [
+              ChatList(
+                imageUrl: 'assets/images/pp-topi.png',
+                message: 'Hello there!',
+                time: 'just now',
+                username: 'Flying Gecko',
+              ),
+              ChatList(
+                imageUrl: 'assets/images/pp-basic.png',
+                message: 'Struggling!',
+                time: '2 min ago',
+                username: 'Green Swamp',
+              ),
+            ],
+          ),
+          Positioned(
+              bottom: 1,
+              right: 1,
+              child: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: kColorOrange,
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
                 ),
-                Text(
-                  'Just now',
-                  style: kBodyTextRubik.copyWith(color: kColorSoftGrey),
-                )
-              ],
-            ),
-            subtitle: Row(
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class ChatList extends StatelessWidget {
+  final String imageUrl;
+  final String username;
+  final String message;
+  final String time;
+  const ChatList({
+    Key? key,
+    required this.imageUrl,
+    required this.username,
+    required this.message,
+    required this.time,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: CircleAvatar(backgroundImage: AssetImage(imageUrl)),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                username,
+                style: kBodyTextRubik.copyWith(
+                    fontSize: 14, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                time,
+                style: kBodyTextRubik.copyWith(color: kColorSoftGrey),
+              )
+            ],
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Row(
               children: [
                 const Icon(Icons.check_sharp),
                 const SizedBox(width: 5),
                 Text(
-                  'Is there a therapy which can cure inner child?',
+                  message,
                   overflow: TextOverflow.ellipsis,
                   style:
                       kBodyTextRubik.copyWith(fontSize: 13, color: kColorBrown),
@@ -39,9 +91,9 @@ class ChatPage extends StatelessWidget {
               ],
             ),
           ),
-          const Divider()
-        ],
-      ),
+        ),
+        const Divider()
+      ],
     );
   }
 }
