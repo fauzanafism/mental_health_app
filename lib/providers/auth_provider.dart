b9b83a1432c454e94e2ac5df024b33b2cf3cd969
fauzanafism <fauzan137@gmail.com>
2023-03-21 16:30:23 +0700

    created auth provider class

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,88 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mental_health_app/models/user_chat.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum Status {
+  uninitialized,
+  authenticated,
+  authenticating,
+  authenticateError,
+  authenticateException,
+  authenticateCanceled
+}
+
+class AuthProvider extends ChangeNotifier {
+  final GoogleSignIn googleSignIn;
+  final FirebaseAuth firebaseAuth;
+  final FirebaseFirestore firebaseFirestore;
+  final SharedPreferences prefs;
+
+  AuthProvider({
+    required this.googleSignIn,
+    required this.firebaseAuth,
+    required this.firebaseFirestore,
+    required this.prefs,
+  });
+
+  Status _status = Status.uninitialized;
+
+  Status get status => _status;
+
+  Future<bool> handleSignIn() async {
+    _status = Status.authenticating;
+    notifyListeners();
+
+    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      GoogleSignInAuthentication? googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+      User? firebaseUser =
+          (await firebaseAuth.signInWithCredential(credential)).user;
+
+      if (firebaseUser != null) {
+        final QuerySnapshot result = await firebaseFirestore
+            .collection("user")
+            .where("id", isEqualTo: firebaseUser.uid)
+            .get();
+        final List<DocumentSnapshot> documents = result.docs;
+        if (documents.isEmpty) {
+          firebaseFirestore.collection("user").doc(firebaseUser.uid).set({
+            "nickname": firebaseUser.displayName,
+            "photoUrl": firebaseUser.photoURL,
+            "id": firebaseUser.uid,
+            "createdAt": DateTime.now().millisecondsSinceEpoch.toString(),
+            "chattingWith": null
+          });
+
+          User? currentUser = firebaseUser;
+          await prefs.setString("id", currentUser.uid);
+          await prefs.setString("nickname", currentUser.displayName ?? "");
+          await prefs.setString("photoUrl", currentUser.photoURL ?? "");
+        } else {
+          DocumentSnapshot documentSnapshot = documents[0];
+          UserChat userChat = UserChat.fromDocument(documentSnapshot);
+
+          await prefs.setString("id", userChat.id);
+          await prefs.setString("nickname", userChat.nickname);
+          await prefs.setString("photoUrl", userChat.photoUrl);
+        }
+        _status = Status.authenticated;
+        notifyListeners();
+        return true;
+      } else {
+        _status = Status.authenticateError;
+        notifyListeners();
+        return false;
+      }
+    } else {
+      _status = Status.authenticateCanceled;
+      notifyListeners();
+      return false;
+    }
+  }
+}
