1c3a184926f8a29c881d66129366e8a4093eeb27
fauzanafism <fauzan137@gmail.com>
2023-03-24 16:32:48 +0700

    created single file for making import easier

---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,8 @@
+export 'chat_page.dart';
+export 'home_page.dart';
+export 'login_page.dart';
+export 'main_page.dart';
+export 'profile_page.dart';
+export 'session_page.dart';
+export 'wellness_page.dart';
+export 'wrapper.dart';
---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,7 @@
+export 'big_box.dart';
+export 'chip.dart';
+export 'loading_view.dart';
+export 'mood.dart';
+export 'post_widget.dart';
+export 'session_widget.dart';
+export 'small_box.dart';
