14c4cc7aa219ef060febbe8c75c098d11c170f40
fauzanafism <fauzan137@gmail.com>
2023-03-24 16:25:26 +0700

    added function for checkin isLoggedIn

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -2,9 +2,13 @@ import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
 import 'package:badges/badges.dart' as badge;
 import 'package:flutter/material.dart';
 import 'package:mental_health_app/common/constant.dart';
+import 'package:mental_health_app/providers/auth_provider.dart';
 import 'package:mental_health_app/ui/pages/chat_page.dart';
+import 'package:mental_health_app/ui/pages/login_page.dart';
+import 'package:mental_health_app/ui/pages/profile_page.dart';
 import 'package:mental_health_app/ui/pages/session_page.dart';
 import 'package:mental_health_app/ui/pages/wellness_page.dart';
+import 'package:provider/provider.dart';
 
 import 'home_page.dart';
 
@@ -17,6 +21,9 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  late AuthProvider authProvider;
+  late String currentUserId;
+
   final List<TabItem> items = [
     const TabItem(icon: Icons.home),
     const TabItem(icon: Icons.videocam),
@@ -32,6 +39,23 @@ class _MainPageState extends State<MainPage> {
     const ChatPage(),
     const WellnessPage(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    authProvider = context.read<AuthProvider>();
+
+    if (authProvider.getFirebaseId()?.isNotEmpty == true) {
+      currentUserId = authProvider.getFirebaseId()!;
+    } else {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const LoginPage(),
+          ),
+          (route) => false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,10 +81,15 @@ class _MainPageState extends State<MainPage> {
             )
           ],
           backgroundColor: Colors.white,
-          leading: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 14),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/pp.jpeg'),
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, ProfilePage.route);
+            },
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/pp.jpeg'),
+              ),
             ),
           ),
           elevation: 0,
