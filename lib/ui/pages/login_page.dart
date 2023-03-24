c49e23e978e6da0a643df1e355dffa9708cd6f1a
fauzanafism <fauzan137@gmail.com>
2023-03-24 16:24:18 +0700

    completed login page and its function

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:mental_health_app/common/constant.dart';
+import 'package:mental_health_app/providers/auth_provider.dart';
 import 'package:mental_health_app/ui/pages/main_page.dart';
+import 'package:mental_health_app/ui/widgets/loading_view.dart';
+import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
   static const String route = '/';
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+    switch (authProvider.status) {
+      case Status.authenticated:
+        Fluttertoast.showToast(msg: "Sign in success");
+        break;
+      case Status.authenticateError:
+        Fluttertoast.showToast(msg: "Sign in fail");
+        break;
+      case Status.authenticateCanceled:
+        Fluttertoast.showToast(msg: "Sign in canceled");
+        break;
+      default:
+        break;
+    }
+
     return Scaffold(
       body: Stack(
         children: [
@@ -29,14 +53,30 @@ class LoginPage extends StatelessWidget {
                             color: Colors.white, fontSize: 46.sp)),
                   ],
                 ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, MainPage.route);
+                TextButton(
+                    onPressed: () async {
+                      authProvider.handleSignIn().then((isSuccess) {
+                        if (isSuccess) {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MainPage(),
+                              ));
+                        }
+                      }).catchError((error, stackTrace) {
+                        Fluttertoast.showToast(msg: error.toString());
+                        authProvider.handleException();
+                      });
                     },
                     child: const Text('LOGIN'))
               ],
             ),
-          )
+          ),
+          Positioned(
+            child: authProvider.status == Status.authenticating
+                ? const LoadingView()
+                : const SizedBox.shrink(),
+          ),
         ],
       ),
     );
