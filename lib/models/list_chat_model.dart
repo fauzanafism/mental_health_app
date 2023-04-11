78109740c48f1f606339e9eff1c21545243751c9
fauzanafism <fauzan137@gmail.com>
2023-04-11 14:57:26 +0700

    added id param for navigation to chat page

--- lib/models/list_chat_model.dart
+++ lib/models/list_chat_model.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ListChatModel {
+  String id;
   String nickname;
   String photoUrl;
   String timestamp;
@@ -10,6 +11,7 @@ class ListChatModel {
 
   ListChatModel(
       {required this.nickname,
+      required this.id,
       required this.photoUrl,
       required this.timestamp,
       required this.content,
@@ -23,18 +25,22 @@ class ListChatModel {
       "timestamp": timestamp,
       "content": content,
       "type": type,
-      "isMe": isMe
+      "isMe": isMe,
+      "id": id
     };
   }
 
   factory ListChatModel.fromJson(Map<String, dynamic> doc) {
     String nickname = doc["nickname"];
+    String id = doc['id'];
     String photoUrl = doc["photoUrl"];
     String timestamp = doc["timestamp"].toString();
     String content = doc["content"];
     int type = doc["type"];
     bool isMe = doc["isMe"];
+
     return ListChatModel(
+        id: id,
         nickname: nickname,
         photoUrl: photoUrl,
         timestamp: timestamp,
