7e61a7510c84259a56b0211c74d28d805c63b37e
fauzanafism <fauzan137@gmail.com>
2023-04-11 14:57:53 +0700

    added recentChat and its connection to db

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:mental_health_app/models/list_chat_model.dart';
 import 'package:mental_health_app/models/message_chat.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,6 +10,10 @@ class ChatProvider {
   final SharedPreferences prefs;
   final FirebaseFirestore firebaseFirestore;
   final FirebaseStorage firebaseStorage;
+
+  List<String> documentList = [];
+
+  List<String> peerList = [];
 
   ChatProvider(
       {required this.prefs,
@@ -35,23 +40,64 @@ class ChatProvider {
         .update(dataNeedUpdate);
   }
 
+  void getDocumentsName(String currentUserId) async {
+    final result = await firebaseFirestore
+        .collection('messages')
+        .where('member', arrayContains: currentUserId)
+        .get();
+    final documents = result.docs;
+    for (var element in documents) {
+      documentList.add(element.id);
+    }
+  }
+
+  // Stream<QuerySnapshot> getLatestChatStream(
+  //     String currentUserId, String peerId) {
+  //   getDocumentsName(currentUserId);
+
+  //   List data = [];
+
+  //   for (var element in documentList) {
+  //     firebaseFirestore
+  //         .collection('messages')
+  //         .doc(element)
+  //         .collection('messages')
+  //         .orderBy('timestamp', descending: true)
+  //         .get()
+  //         .then((value) => data.add(value));
+  //   }
+  // }
+
+  Stream<DocumentSnapshot> getRecentChatStream(String currentUserId) {
+    return firebaseFirestore
+        .collection('recentChat')
+        .doc(currentUserId)
+        .snapshots();
+  }
+
   Stream<QuerySnapshot> getChatStream(String groupChatId, int limit) {
     return firebaseFirestore
         .collection("messages")
         .doc(groupChatId)
-        .collection(groupChatId)
+        .collection("messages")
         .orderBy('timestamp', descending: true)
         .limit(20)
         .snapshots();
   }
 
   void sendMessage(String content, int type, String groupChatId,
-      String currentUserId, String peerId) {
+      String currentUserId, String peerId) async {
     DocumentReference documentReference = firebaseFirestore
         .collection("messages")
         .doc(groupChatId)
-        .collection(groupChatId)
+        .collection("messages")
         .doc(DateTime.now().millisecondsSinceEpoch.toString());
+
+    DocumentReference recentChatUser =
+        firebaseFirestore.collection("recentChat").doc(currentUserId);
+
+    DocumentReference recentChatPeer =
+        firebaseFirestore.collection("recentChat").doc(peerId);
 
     MessageChat messageChat = MessageChat(
         idFrom: currentUserId,
@@ -60,8 +106,79 @@ class ChatProvider {
         content: content,
         type: type);
 
+    DocumentSnapshot documentPeer =
+        await firebaseFirestore.collection('user').doc(peerId).get();
+
+    var peerProfile = documentPeer.data() as Map<String, dynamic>;
+
+    bool isMe = true;
+
+    ListChatModel listChatModel = ListChatModel(
+        id: peerId,
+        nickname: peerProfile['nickname'],
+        photoUrl: peerProfile['photoUrl'],
+        timestamp: DateTime.now().millisecondsSinceEpoch.toString(),
+        content: content,
+        type: type,
+        isMe: isMe);
+
+    // Map<String, dynamic> docField = {};
+
+    // await firebaseFirestore
+    //     .collection("messages")
+    //     .doc(groupChatId)
+    //     .get()
+    //     .then((value) {
+    //   if (value.data() != null) {
+    //     docField = value.data() as Map<String, dynamic>;
+    //   } else {
+    //     docField = {};
+    //   }
+    // });
+
+    // if (docField.isEmpty) {
+    //   FirebaseFirestore.instance.runTransaction((transaction) async {
+    //     transaction
+    //         .set(firebaseFirestore.collection("messages").doc(groupChatId), {
+    //       "member": [messageChat.idTo, messageChat.idFrom]
+    //     });
+    //   });
+    // }
+
+    // Input data message ke collection message
     FirebaseFirestore.instance.runTransaction((transaction) async {
       transaction.set(documentReference, messageChat.toJson());
+    });
+
+    var listDoc = await firebaseFirestore
+        .collection('recentChat')
+        .get()
+        .then((value) => value.docs.map((e) => e.id).toList());
+    if (!listDoc.contains(currentUserId)) {
+      FirebaseFirestore.instance.runTransaction((transaction) async {
+        transaction.set(recentChatUser, {peerId: {}});
+      });
+      print('ini jalan');
+    }
+
+    // Input data ke collection recentChat user
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      transaction.update(recentChatUser, {peerId: listChatModel.toJson()});
+    });
+
+    isMe = !isMe;
+
+    if (!listDoc.contains(peerId)) {
+      FirebaseFirestore.instance.runTransaction((transaction) async {
+        transaction.set(recentChatPeer, {currentUserId: {}});
+      });
+      print('ini jalan');
+    }
+
+    // Input data ke collection recentChat peer
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      transaction
+          .update(recentChatPeer, {currentUserId: listChatModel.toJson()});
     });
   }
 }
