38b3bf6212317e91068845d3f56773c75e818e0d
fauzanafism <fauzan137@gmail.com>
2023-06-20 10:04:34 +0700

    added notif helper

--- lib/utils/notification_helper.dart
+++ lib/utils/notification_helper.dart
@@ -1,82 +1,82 @@
-import 'dart:convert';
-import 'dart:math';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:mental_health_app/utils/navigation.dart';
-import 'package:rxdart/rxdart.dart';
+// import 'dart:convert';
+// import 'dart:math';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:mental_health_app/utils/navigation.dart';
+// import 'package:rxdart/rxdart.dart';
 
-final selectNotificationSubject = BehaviorSubject<String>();
+// final selectNotificationSubject = BehaviorSubject<String>();
 
-class NotificationHelper {
-  static NotificationHelper? _instance;
+// class NotificationHelper {
+//   static NotificationHelper? _instance;
 
-  NotificationHelper._internal() {
-    _instance = this;
-  }
+//   NotificationHelper._internal() {
+//     _instance = this;
+//   }
 
-  factory NotificationHelper() => _instance ?? NotificationHelper._internal();
+//   factory NotificationHelper() => _instance ?? NotificationHelper._internal();
 
-  Future<void> initNotifications(
-      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
-    var initializationSettingsAndroid =
-        const AndroidInitializationSettings('app_icon');
+//   Future<void> initNotifications(
+//       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+//     var initializationSettingsAndroid =
+//         const AndroidInitializationSettings('app_icon');
 
-    var initializationSettingsIOS = const DarwinInitializationSettings(
-      requestAlertPermission: false,
-      requestBadgePermission: false,
-      requestSoundPermission: false,
-    );
+//     var initializationSettingsIOS = const DarwinInitializationSettings(
+//       requestAlertPermission: false,
+//       requestBadgePermission: false,
+//       requestSoundPermission: false,
+//     );
 
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+//     var initializationSettings = InitializationSettings(
+//         android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
 
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onDidReceiveNotificationResponse: (NotificationResponse details) async {
-      final payload = details.payload;
-      selectNotificationSubject.add(payload ?? 'empty payload');
-    });
-  }
+//     await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+//         onDidReceiveNotificationResponse: (NotificationResponse details) async {
+//       final payload = details.payload;
+//       selectNotificationSubject.add(payload ?? 'empty payload');
+//     });
+//   }
 
-  Future<void> showNotification(
-      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
-      RestaurantList restaurantList) async {
-    var channelId = "1";
-    var channelName = "channel_01";
-    var channelDescription = "zan restaurant";
+//   Future<void> showNotification(
+//       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+//       RestaurantList restaurantList) async {
+//     var channelId = "1";
+//     var channelName = "channel_01";
+//     var channelDescription = "zan restaurant";
 
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        channelId, channelName,
-        channelDescription: channelDescription,
-        importance: Importance.max,
-        priority: Priority.high,
-        ticker: 'ticker',
-        styleInformation: const DefaultStyleInformation(true, true));
+//     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+//         channelId, channelName,
+//         channelDescription: channelDescription,
+//         importance: Importance.max,
+//         priority: Priority.high,
+//         ticker: 'ticker',
+//         styleInformation: const DefaultStyleInformation(true, true));
 
-    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
-        android: androidPlatformChannelSpecifics,
-        iOS: iOSPlatformChannelSpecifics);
+//     var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
+//     var platformChannelSpecifics = NotificationDetails(
+//         android: androidPlatformChannelSpecifics,
+//         iOS: iOSPlatformChannelSpecifics);
 
-    var titleNotification = "<b>Restaurant of the day!!</b>";
+//     var titleNotification = "<b>Restaurant of the day!!</b>";
 
-    var list = restaurantList.restaurants;
-    var random = Random().nextInt(list.length);
+//     var list = restaurantList.restaurants;
+//     var random = Random().nextInt(list.length);
 
-    var titleNews = list[random].name;
+//     var titleNews = list[random].name;
 
-    await flutterLocalNotificationsPlugin.show(
-        0, titleNotification, titleNews, platformChannelSpecifics,
-        payload: json.encode(restaurantList.toJson()));
-  }
+//     await flutterLocalNotificationsPlugin.show(
+//         0, titleNotification, titleNews, platformChannelSpecifics,
+//         payload: json.encode(restaurantList.toJson()));
+//   }
 
-  void configureSelectNotificationSubject(String route) {
-    selectNotificationSubject.stream.listen(
-      (String payload) async {
-        var data = RestaurantList.fromJson(json.decode(payload));
-        var list = data.restaurants;
-        var random = Random().nextInt(list.length);
-        var restaurant = data.restaurants[random];
-        Navigation.intentWithData(route, restaurant);
-      },
-    );
-  }
-}
+//   void configureSelectNotificationSubject(String route) {
+//     selectNotificationSubject.stream.listen(
+//       (String payload) async {
+//         var data = RestaurantList.fromJson(json.decode(payload));
+//         var list = data.restaurants;
+//         var random = Random().nextInt(list.length);
+//         var restaurant = data.restaurants[random];
+//         Navigation.intentWithData(route, restaurant);
+//       },
+//     );
+//   }
+// }
