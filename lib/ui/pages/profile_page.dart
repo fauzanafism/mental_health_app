714195fb7a5898b2f92ac35d67e626a5ac41f897
fauzanafism <fauzan137@gmail.com>
2023-06-22 09:58:52 +0700

    added username and profile photo

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:mental_health_app/common/constant.dart';
 import 'package:mental_health_app/providers/auth_provider.dart';
 import 'package:mental_health_app/ui/pages/pages.dart';
 import 'package:provider/provider.dart';
@@ -12,8 +14,20 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  late AuthProvider authProvider;
+  late String username;
+  late String userPhoto;
+
+  @override
+  void initState() {
+    super.initState();
+    authProvider = context.read<AuthProvider>();
+
+    username = authProvider.getUserNickname()!;
+    userPhoto = authProvider.getUserPhoto()!;
+  }
+
   Future<void> handleSignOut() async {
-    AuthProvider authProvider = context.read<AuthProvider>();
     authProvider.handleSignOut();
     Navigator.pushAndRemoveUntil(
         context,
@@ -26,12 +40,50 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'Profile',
+          style: kTitle,
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
       body: Center(
-        child: TextButton(
-          onPressed: () {
-            handleSignOut();
-          },
-          child: const Text("Logout"),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: CachedNetworkImageProvider(userPhoto),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              username,
+              style: kTitle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Saya suka ngoding',
+              style: kSubtitle,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: kColorOrange),
+              onPressed: () {
+                handleSignOut();
+              },
+              child: const Text("Logout"),
+            ),
+          ],
         ),
       ),
     );
